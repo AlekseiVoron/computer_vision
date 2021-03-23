16461e3d98ee44082040bb5503a20e861622e2a2
ThePeacemaker777 <IPIDlol777*>
2021-03-23 09:41:33 +0400

    The second

--- task3.py
+++ task3.py
@@ -18,35 +18,38 @@ if __name__ == '__main__':
     img = cv2.imread(IMAGE, cv2.IMREAD_GRAYSCALE)
     img_color = cv2.imread(IMAGE)
 
-    equ = cv2.equalizeHist(img)
-    equ_res = np.hstack((img, equ))
+    img_lab = cv2.cvtColor(img_color, cv2.COLOR_BGR2LAB)
+    l, a, b = cv2.split(img_lab)
+
+    equ_l = cv2.merge([cv2.equalizeHist(l), a, b])
+    equ_res = np.hstack((img_color, cv2.cvtColor(equ_l, cv2.COLOR_LAB2BGR)))
     cv2.imshow('equ', equ_res)
 
-    clahe = cv2.createCLAHE(4, (8, 8)).apply(img)
-    clahe_res = np.hstack((img, clahe))
+    clahe_l = cv2.merge([cv2.createCLAHE(4, (8, 8)).apply(l), a, b])
+    clahe_res = np.hstack((img_color, cv2.cvtColor(clahe_l, cv2.COLOR_LAB2BGR)))
     cv2.imshow('clahe', clahe_res)
 
-    gauss = cv2.GaussianBlur(img_color, (5, 5), 0)
-    gauss_res = np.hstack((img_color, gauss))
-    cv2.imshow('gauss', gauss_res)
-
-    grad_x = cv2.Sobel(img, cv2.CV_16S, 1, 0)
-    grad_y = cv2.Sobel(img, cv2.CV_16S, 0, 1)
-    abs_grad_x = cv2.convertScaleAbs(grad_x)
-    abs_grad_y = cv2.convertScaleAbs(grad_y)
-    sobel = cv2.addWeighted(abs_grad_x, 0.5, abs_grad_y, 0.5, 0)
-    sobel_res = np.hstack((img, sobel))
-    cv2.imshow('sobel', sobel_res)
-
-    laplacian = cv2.convertScaleAbs(cv2.Laplacian(img, cv2.CV_16S, ksize=3))
-    laplacian_res = np.hstack((img, laplacian))
-    cv2.imshow('laplacian', laplacian_res)
-
-    src1 = cv2.imread(IMG1)
-    src2 = cv2.imread(IMG2)
-    alpha = 0.5
-    beta = (1.0 - alpha)
-    alpha_blending = cv2.addWeighted(src1, alpha, src2, beta, 0.0)
-    cv2.imshow('alpha_blending', alpha_blending)
+    # gauss = cv2.GaussianBlur(img_color, (5, 5), 0)
+    # gauss_res = np.hstack((img_color, gauss))
+    # cv2.imshow('gauss', gauss_res)
+    #
+    # grad_x = cv2.Sobel(img, cv2.CV_16S, 1, 0)
+    # grad_y = cv2.Sobel(img, cv2.CV_16S, 0, 1)
+    # abs_grad_x = cv2.convertScaleAbs(grad_x)
+    # abs_grad_y = cv2.convertScaleAbs(grad_y)
+    # sobel = cv2.addWeighted(abs_grad_x, 0.5, abs_grad_y, 0.5, 0)
+    # sobel_res = np.hstack((img, sobel))
+    # cv2.imshow('sobel', sobel_res)
+    #
+    # laplacian = cv2.convertScaleAbs(cv2.Laplacian(img, cv2.CV_16S, ksize=3))
+    # laplacian_res = np.hstack((img, laplacian))
+    # cv2.imshow('laplacian', laplacian_res)
+    #
+    # src1 = cv2.imread(IMG1)
+    # src2 = cv2.imread(IMG2)
+    # alpha = 0.5
+    # beta = (1.0 - alpha)
+    # alpha_blending = cv2.addWeighted(src1, alpha, src2, beta, 0.0)
+    # cv2.imshow('alpha_blending', alpha_blending)
 
     cv2.waitKey(0)
--- task4.py
+++ task4.py
@@ -1,11 +1,74 @@
 import cv2
+import numpy as np
+
+PYRAMIDS_DEPTH = 6
+
+FOREGROUND = 'car.png'
+BACKGROUND = 'road.jpg'
+
+WHITE = [255, 255, 255]
+BLACK = [0, 0, 0]
+
+
+def make_mask(input_image):
+    copy_image = np.copy(input_image)
+    for i in range(len(copy_image)):
+        for j in range(len(copy_image[i])):
+            copy_image[i][j] = WHITE if copy_image[i][j].any() else BLACK
+    return copy_image
 
 
 if __name__ == '__main__':
-    def loading_displaying_saving():
-        img = cv2.imread('girl.jpg', cv2.IMREAD_GRAYSCALE)
-        cv2.imshow('girl', img)
-        cv2.waitKey(0)
-        cv2.imwrite('graygirl.jpg', img)
+    A = cv2.imread(FOREGROUND)
+    B = cv2.imread(BACKGROUND)
+    m = make_mask(A)
 
+    # assume mask is float32 [0,1]
 
+    # generate Gaussian pyramid for A,B and mask
+    GA = A.copy()
+    GB = B.copy()
+    GM = m.copy()
+    gpA = [GA]
+    gpB = [GB]
+    gpM = [GM]
+    for i in range(PYRAMIDS_DEPTH):
+        GA = cv2.pyrDown(GA)
+        GB = cv2.pyrDown(GB)
+        GM = cv2.pyrDown(GM)
+        gpA.append(np.float32(GA))
+        gpB.append(np.float32(GB))
+        gpM.append(np.float32(GM))
+
+    # generate Laplacian Pyramids for A,B and masks
+    lpA = [gpA[PYRAMIDS_DEPTH - 1]]  # the bottom of the Lap-pyr holds the last (smallest) Gauss level
+    lpB = [gpB[PYRAMIDS_DEPTH - 1]]
+    gpMr = [gpM[PYRAMIDS_DEPTH - 1]]
+    for i in range(PYRAMIDS_DEPTH - 1, 0, -1):
+        # Laplacian: subtarct upscaled version of lower level from current level
+        # to get the high frequencies
+        temp = cv2.pyrUp(gpA[i])
+        LA = np.subtract(gpA[i - 1], cv2.pyrUp(gpA[i]))
+        LB = np.subtract(gpB[i - 1], cv2.pyrUp(gpB[i]))
+        lpA.append(LA)
+        lpB.append(LB)
+        gpMr.append(gpM[i - 1])  # also reverse the masks
+
+    # Now blend images according to mask in each level
+    LS = []
+    for la, lb, gm in zip(lpA, lpB, gpMr):
+        ls = la * gm + lb * (1.0 - gm)
+        LS.append(ls)
+
+    # now reconstruct
+    ls_ = LS[0]
+    for i in range(1, PYRAMIDS_DEPTH):
+        ls_ = cv2.pyrUp(ls_)
+        ls_ = cv2.add(ls_, LS[i])
+
+    cv2.imshow('Foreground', A)
+    cv2.imshow('Background', B)
+    cv2.imshow('Mask', m)
+    cv2.imshow('Laplacian_pyramid_blending', ls_)
+
+    cv2.waitKey(0)
